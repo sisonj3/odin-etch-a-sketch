dbbce27d329de127f48d21c2fc7ba398a156677c
sisonj <98570174+sisonj3@users.noreply.github.com>
2023-09-22 15:30:38 -0700

    Made the new grid button delete the old grid and create a new grid based on the number value inputted by the user when prompted

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 const container = document.querySelector('.container');
+const newGridBtn = document.querySelector('.newGrid');
 
-for(let i = 0; i < 16; i++){
-    const row = document.createElement('div');
+// Allow the user to create a custom sized grid
+newGridBtn.addEventListener('click', promptNewGrid);
 
-    row.classList.add('row');
+newGrid(16);
 
-    for(let j = 0; j < 16; j++) {
-        const newDiv = document.createElement('div');
-        newDiv.classList.add('square');
+function promptNewGrid() {
+    let size = prompt('Enter grid size (NxN)');
 
-        newDiv.addEventListener('mouseover', (e) => {
-            e.target.style.backgroundColor = "black";
-        });
+    if(size <= 0 || isNaN(size))
+        return;
 
-        row.appendChild(newDiv);
+    // Remove current grid before replacing it
+    const rows = document.querySelectorAll('.row');
+    rows.forEach(row => { container.removeChild(row);});
+
+    newGrid(size);
+}
+
+function newGrid(size) {
+    for(let i = 0; i < size; i++){
+        const row = document.createElement('div');
+    
+        row.classList.add('row');
+    
+        for(let j = 0; j < size; j++) {
+            const newDiv = document.createElement('div');
+            newDiv.classList.add('square');
+    
+            newDiv.addEventListener('mouseover', (e) => {
+                e.target.style.backgroundColor = "black";
+            });
+    
+            row.appendChild(newDiv);
+        }
+    
+        container.appendChild(row);
     }
-
-    container.appendChild(row);
 }
